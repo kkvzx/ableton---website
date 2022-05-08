eea0b840da36fa450a18b7e198b67bee79e8f110
kkvzx <64148572+kkvzx@users.noreply.github.com>
2022-05-08 22:10:16 +0200

    Initial commit

---
+++ dist/app.js
@@ -0,0 +1,50 @@
+const navToggle = document.querySelectorAll(".body-nav-toggle");
+const mainBtns = document.querySelectorAll(".main-btn");
+const pageContent = document.querySelectorAll(".main-content");
+const main = document.querySelector(".main");
+// functions
+const changeTab = (e) => {
+    const id = e.target.dataset.id;
+    const objectClass = e.target.classList.contains("body-nav-toggle");
+    if (objectClass) {
+        if (id) {
+            // remove active from all the buttons
+            for (let i of navToggle) {
+                i.classList.remove("active");
+            }
+            e.target.classList.add("active");
+        }
+    }
+};
+const changePage = (e) => {
+    const id = e.target.dataset.id;
+    const objectClass = e.target.classList.contains("main-btn");
+    if (objectClass) {
+        if (id) {
+            // remove active fromm all the btns
+            for (let i of mainBtns) {
+                i.classList.remove("active");
+            }
+            e.target.classList.add("active");
+        }
+    }
+};
+const changeContent = (e) => {
+    const id = e.target.dataset.id;
+    const objectClass = e.target.classList.contains("body-nav-toggle");
+    if (objectClass) {
+        if (id) {
+            // remove active from all the elements
+            for (let i of pageContent) {
+                i.classList.remove("activeLinks");
+            }
+            const element = document.getElementById(id);
+            element.classList.add("activeLinks");
+        }
+    }
+};
+main.addEventListener("click", (e) => {
+    changeTab(e);
+    changePage(e);
+    changeContent(e);
+});
---
+++ src/app.ts
@@ -0,0 +1,64 @@
+const navToggle: NodeListOf<Element> =
+  document.querySelectorAll(".body-nav-toggle");
+const mainBtns: NodeListOf<Element> = document.querySelectorAll(".main-btn");
+const pageContent: NodeListOf<Element> =
+  document.querySelectorAll(".main-content");
+const main: HTMLElement = document.querySelector(".main");
+
+// functions
+
+const changeTab = (e: Event): void => {
+  const id: string = (e.target as HTMLInputElement).dataset.id;
+  const objectClass = (e.target as HTMLInputElement).classList.contains(
+    "body-nav-toggle"
+  );
+  if (objectClass) {
+    if (id) {
+      // remove active from all the buttons
+      for (let i of navToggle) {
+        i.classList.remove("active");
+      }
+      (e.target as HTMLInputElement).classList.add("active");
+    }
+  }
+};
+
+const changePage = (e: Event): void => {
+  const id = (e.target as HTMLInputElement).dataset.id;
+  const objectClass = (e.target as HTMLInputElement).classList.contains(
+    "main-btn"
+  );
+  if (objectClass) {
+    if (id) {
+      // remove active fromm all the btns
+      for (let i of mainBtns) {
+        i.classList.remove("active");
+      }
+      (e.target as HTMLInputElement).classList.add("active");
+    }
+  }
+};
+
+const changeContent = (e: Event): void => {
+  const id: string = (e.target as HTMLInputElement).dataset.id;
+  const objectClass = (e.target as HTMLInputElement).classList.contains(
+    "body-nav-toggle"
+  );
+
+  if (objectClass) {
+    if (id) {
+      // remove active from all the elements
+      for (let i of pageContent) {
+        i.classList.remove("activeLinks");
+      }
+      const element: HTMLElement = document.getElementById(id);
+      element.classList.add("activeLinks");
+    }
+  }
+};
+
+main.addEventListener("click", (e: Event) => {
+  changeTab(e);
+  changePage(e);
+  changeContent(e);
+});
